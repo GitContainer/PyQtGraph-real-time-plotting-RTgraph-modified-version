4d9b5cd64524fb7d02378d1a9c90f3912f0488cf
LatinAmericanProgramer <ing.kevin@hotmail.com>
2018-08-09 19:37:46 +0100

    Plotting sine & cosine signals

--- mainWindowPPG.py
+++ mainWindowPPG.py
@@ -1,124 +1,66 @@
 """
-Example to plot data from a function in real time
-Ref: https://github.com/ssepulveda
-Ref: https://github.com/ssepulveda/RTGraph/tree/oldRTGraph
+Basic example to plot data from a function in real time
 Created on Thu Dec 21 12:07:49 2017
 by the author: Kevin Machado Gamboa
-Ref: https://github.com/LatinAmericanProgramer/PyQtGraph-real-time-plotting-RTgraph-modified-version
 Contct: ing.kevin@hotmail.com
 Modified on Sat Jan 06 08:50:23 2018
+Ref1: https://github.com/ssepulveda
+Ref2: https://github.com/ssepulveda/RTGraph/tree/oldRTGraph
+Ref3: https://github.com/LatinAmericanProgramer/PyQtGraph-real-time-plotting-RTgraph-modified-version
 """
 import sys
-import numpy as np
-from time import time
-from collections import deque
-from multiprocessing import Queue
-# Librery for the management of Qt v5. UI platform
-from PyQt5.uic import loadUi
 from PyQt5 import QtWidgets, QtCore
-from PyQt5.QtWidgets import QApplication, QMainWindow, QSlider
-# graphics and GUI library
+from PyQt5.uic import loadUi
+from PyQt5.QtWidgets import QApplication, QMainWindow
 from pyqtgraph import GraphicsLayoutWidget
+from multiprocessing import Queue
+from collections import deque
+
+from time import time
+import numpy as np
 
 
+##
 # @brief Buffer size for the data (number of points in the plot)
 N_SAMPLES = 100
-# @brief Update time of the plot, in ms
-PLOT_UPDATE_TIME = 1
-# @brief Point to update in each redraw
-PLOT_UPDATE_POINTS = -1
+##s
+# @brief Point to update in each redrawIR
+PLOT_UPDATE_POINTS = -2
 
 class mainWindow(QMainWindow):
+        # Metodo constructor de la clase
     def __init__(self):
         #Inicia el objeto QMainWindow
         QMainWindow.__init__(self)
-        # Loads an .ui file 
+        #carga la configuracion del archivo .ui en el objeto
         loadUi("mainWindowPPG.ui",self)
+        # Loads everything about mainWindowPPG configuration
+        self.setupUI()
         # Shared variables, initial values
         self.queue = Queue(N_SAMPLES)
         self.dataR = deque([], maxlen=N_SAMPLES)
         self.dataIR = deque([], maxlen=N_SAMPLES)
         self.TIME = deque([], maxlen=N_SAMPLES)
-        
         self._plt = None
         self._timer_plot = None
         self.plot_colors = ['#0072bd', '#d95319']
-        # Spo2 signal parameters
-        self.timestamp = 0.0
-        self.ampR = 1.0814       # amplitud for Red signal
-        self.minR = 0.0   # Desplacement from zero for Red signal
-        self.ampIR = 1.0814       # amplitud for InfraRed signal
-        self.minIR = 1.0   # Desplacement from zero for Red signal
-        # UI connectors
-        self.HRBox.valueChanged.connect(self._initial)        
-        self.spo2sl.valueChanged.connect(self.spo2sl_change)
-        
-        # Configurations
-        self.setupUI()
-        self._enable_ui(True)       
+        # configures
         self._configure_plot()
         self._configure_timers()
-        self.buttons()
-        self._initial()
+        self._configure_signals()
 
-    def start(self):
-        """
-        This function works when the start button is clicked
-        It generates a t0 time and activates the Qt timer which connects to update_plot
-        :return:
-        """
-        self._enable_ui(False)
-        self.timestamp= time()
-        self._timer_plot.start(PLOT_UPDATE_TIME)
-        
-    def stop(self):
-        """
-        This function works when the stop button is clicked
-        it stop the timer and resets the buffers
-        """
-        self._enable_ui(True)
-        self._timer_plot.stop()  
-        self.reset_buffers()
-    
-    def _update_plot(self):
-        """
-        Updates and redraws the graphics in the plot.
-        :return:
-        """
-        # generates the time
-        self.tPPG = time() - self.timestamp
-        self.sR, self.sIR = self.ppg_parameters(self.minR, self.ampR, self.minIR, self.ampIR, self.tPPG)
-        self.queue.put([self.tPPG,self.sR,self.sIR])
-        data = self.queue.get(True, 1)
-        # store data into variables 
-        self.TIME.append(data[0])
-        self.dataR.append(data[1])
-        self.dataIR.append(data[2])
-        # Draw new data
-        self._plt.clear()
-        self._plt.plot(x=list(self.TIME)[-PLOT_UPDATE_POINTS:], y=list(self.dataR)[-PLOT_UPDATE_POINTS:], pen=self.plot_colors[1])
-        self._plt.plot(x=list(self.TIME)[-PLOT_UPDATE_POINTS:], y=list(self.dataIR)[-PLOT_UPDATE_POINTS:], pen=self.plot_colors[0])
-                
+# Loads everything about mainWindowPPG configuration        
     def setupUI(self):
         """
-       Configures everything regarding the UI
+       Configures everything about the mainWindow
         """
-        self.plt = GraphicsLayoutWidget(self.centralwidget)
+        self.plt = GraphicsLayoutWidget(self.centralwidget) # Bringing my plot wiindow as plt
         self.plt.setAutoFillBackground(False)
-        self.plt.setStyleSheet("background-color: rgb(0,0,0)")
+        self.plt.setStyleSheet("border: 0px;")
         self.plt.setFrameShape(QtWidgets.QFrame.StyledPanel)
         self.plt.setFrameShadow(QtWidgets.QFrame.Plain)
         self.plt.setLineWidth(0)
-        self.Layout_graphs.addWidget(self.plt, 0, 0, 1, 1) # Set the plotting squared into UI graph
-        # Defult Heart Rate Widget configuration
-        self.HRBox.setRange(50,150)
-        self.HRBox.setProperty("value", 80)
-        # SpO2 Slider configuration
-        self.spo2sl.setMaximum(100)
-        self.spo2sl.setMinimum(50)
-        self.spo2sl.setValue(100)
-        self.spo2sl.setTickPosition(QSlider.TicksBelow)
-        self.spo2sl.setTickInterval(1)
+        self.Layout_graphs.addWidget(self.plt, 0, 0, 1, 1) # Set the plotting squared graph
     
     def _configure_plot(self):
         """
@@ -129,7 +71,7 @@ class mainWindow(QMainWindow):
         self.plt.setAntialiasing(True)
         self._plt = self.plt.addPlot(row=1, col=1)
         self._plt.setLabel('bottom', "Time", "s")
-        self._plt.showGrid(x=False, y=True)
+        pass
     
     def _configure_timers(self):
         """
@@ -138,74 +80,70 @@ class mainWindow(QMainWindow):
         """
         self._timer_plot = QtCore.QTimer(self) # gives _timer_plot the attribute of QtCore.QTimer
         self._timer_plot.timeout.connect(self._update_plot)  # connects with _update_plot method  
-    
-    def ppg_parameters(self, minR, ampR, minIR, ampIR, t):
-        """
-        Store the function of two signals - e.g PPG Red and Infrared channel signals
-        We can also put here a sine, cosine, etc.
-        """
-        # Spo2 signal parameters
-        HR = float(self.HRBox.value())
-        #speed=float(self.ui.cBox_Speed.currentText())
-        f= HR * (1/60)
-        # Spo2 Red signal function
-        self.sR= minR + ampR * (0.05*np.sin(2*np.pi*t*3*f)
-        + 0.4*np.sin(2*np.pi*t*f) + 0.25*np.sin(2*np.pi*t*2*f+45))
-        # Spo2 InfraRed signal function
-        self.sIR= minIR + ampIR * (0.05*np.sin(2*np.pi*t*3*f)
-        + 0.4*np.sin(2*np.pi*t*f) + 0.25*np.sin(2*np.pi*t*2*f+45))
-        
-        return self.sR, self.sIR
-    
-    def _initial(self):
-        """
-        contain the initial figure in the UI
-        """
-        t=np.linspace(0,1,100);
-        sR,sIR = self.ppg_parameters(self.minR, self.ampR, self.minIR, self.ampIR, t)
-
-        self._plt.clear()
-        self._plt.plot(x=list(t)[-PLOT_UPDATE_POINTS:], y=list(sR)[-PLOT_UPDATE_POINTS:], pen=self.plot_colors[1])
-        self._plt.plot(x=list(t)[-PLOT_UPDATE_POINTS:], y=list(sIR)[-PLOT_UPDATE_POINTS:], pen=self.plot_colors[0])
-    
-    def spo2values(self):
-        """
-        It have the list of SpO2 values vs the R value
-        """
         pass
     
-    def buttons(self):
+    def _update_plot(self):
+        """
+        Updates and redraws the graphics in the plot.
+        This function us connected to the timeout signal of a QTimer.
+        :return:
+        """
+        # Spo2 signal parameters
+        f=2
+        amp1 = 0.5       # amplitud for Sine signal
+        zeroDes1 = 0.5413   # Desplacement from zero for Sine signal
+        amp2 = 0.5       # amplitud for Cosine signal
+        zeroDes2 = 1.5413   # Desplacement from zero for Cosine signal
+        # generate the time
+        tsignal = time() - self.timestamp
+        # Sine signal function
+        sR= zeroDes1 + amp1 * 0.8 *np.sin(2*np.pi*tsignal*3*f)
+        # Cosine signal function
+        sIR= zeroDes2 + amp2 * 0.8 *np.cos(2*np.pi*tsignal*3*f)
+        # put the data generate (time & signal) into queue
+        self.queue.put([tsignal,sR,sIR])
+        # get the data generate into queue
+        data = self.queue.get(True, 1)
+        # store data into variables 
+        self.TIME.append(data[0])
+        self.dataR.append(data[1])
+        self.dataIR.append(data[2])
+        # Draw new data
+        self._plt.clear()
+        self._plt.plot(x=list(self.TIME)[-PLOT_UPDATE_POINTS:], y=list(self.dataR)[-PLOT_UPDATE_POINTS:], pen=self.plot_colors[1])
+        self._plt.plot(x=list(self.TIME)[-PLOT_UPDATE_POINTS:], y=list(self.dataIR)[-PLOT_UPDATE_POINTS:], pen=self.plot_colors[0])
+    
+    def _configure_signals(self):
         """
         Configures the connections between signals and UI elements.
+        :return:
         """
         self.startButton.clicked.connect(self.start)
         self.stopButton.clicked.connect(self.stop)
+
+    def start(self):
+        """
+        Starts the acquisition of the selected serial port.
+        This function is connected to the clicked signal of the Start button.
+        :return:
+        """
+        self.timestamp= time()
+        self._timer_plot.start(16)
+        
+    def stop(self):
+        """
+        Stops the acquisition of the selected serial port.
+        This function is connected to the clicked signal of the Stop button.
+        :return:
+        """
+        self._timer_plot.stop()  # stop the Qt timer
+        self.reset_buffers()     # Go to reset the vector containing values
     
     def reset_buffers(self):
-        """
-        Clear everything into the vectors that have the signals
-        """
         self.dataR.clear()
         self.dataIR.clear()
         self.TIME.clear()
-        
-    def spo2sl_change(self):
-        """
-        Change the value of the SpO2 when movind the slider
-        """
-        spo2value = str(self.spo2sl.value())
-        self.showSpo2.setText(spo2value)
-        
-    def _enable_ui(self, enabled):
-        """
-        Enable touching the buttons in the UI
-        """
-        self.startButton.setEnabled(enabled)
-        self.spo2sl.setEnabled(enabled)
-        self.showSpo2.setEnabled(enabled)
-        self.HRBox.setEnabled(enabled)
-        self.stopButton.setEnabled(not enabled)
-        
+  
 #Instancia para iniciar una aplicacion en windows
 app = QApplication(sys.argv)
 #debemos crear un objeto para la clase creada arriba
